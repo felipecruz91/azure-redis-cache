db4c36827d084460e4d4b3aa1144f3fcd086cf45
Felipe Cruz <fcz@kamstrup.com>
2019-05-20 18:53:28 +0200

    Initial commit.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var cacheConnection = ConfigurationManager.AppSettings["CacheConnection"];
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        private static void Main(string[] args)
+        {
+            // Connection refers to a property that returns a ConnectionMultiplexer
+            // as shown in the previous example.
+            var cache = LazyConnection.Value.GetDatabase();
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            var cacheCommand = "PING";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + cache.Execute(cacheCommand));
+
+            // Simple get and put of integral data types into the cache
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message"));
+
+            cacheCommand = "SET Message \"Hello! The cache is working from a .NET console app!\"";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringSet()");
+            Console.WriteLine("Cache response : " +
+                              cache.StringSet("Message", "Hello! The cache is working from a .NET console app!"));
+
+            // Demonstrate "SET Message" executed as expected...
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message"));
+
+            // Get the client list, useful to see if connection list is growing...
+            cacheCommand = "CLIENT LIST";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : \n" + cache.Execute("CLIENT", "LIST").ToString().Replace("id=", "id="));
+
+            LazyConnection.Value.Dispose();
+        }
+    }
+}
